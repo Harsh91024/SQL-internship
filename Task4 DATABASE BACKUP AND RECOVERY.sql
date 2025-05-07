40a002f956982afd67c9e8b6cdb2f00b582ec477
Harsh91024 <harshhkk2001@gmail.com>
2025-05-07 21:38:01 +0530

    Update Task4 DATABASE BACKUP AND RECOVERY.sql
    .

--- Task4 DATABASE BACKUP AND RECOVERY.sql
+++ Task4 DATABASE BACKUP AND RECOVERY.sql
@@ -1,18 +1,18 @@
-# Task 4: Database Backup and Recovery (SQLite)
+-- # Task 4: Database Backup and Recovery (SQLite)
 
-## Objective:
-Back up and restore SQLite database using DBeaver.
+-- ## Objective:
+-- Back up and restore SQLite database using DBeaver.
 
-## Backup Methods:
-- Copied original `.sqlite` file as a full backup
-- Exported SQL script `backup_task4.sql` (includes table structure and data)
+-- ## Backup Methods:
+-- - Copied original `.sqlite` file as a full backup
+-- - Exported SQL script `backup_task4.sql` (includes table structure and data)
 
-## Recovery Steps:
-- Simulated failure by dropping tables
-- Restored data by executing `backup_task4.sql` in DBeaver
+-- ## Recovery Steps:
+-- - Simulated failure by dropping tables
+-- - Restored data by executing `backup_task4.sql` in DBeaver
 
-## Tools:
-- SQLite
-- DBeaver
+-- ## Tools:
+-- - SQLite
+-- - DBeaver
 
-## Status: ✅ Backup & Recovery successful
+-- ## Status: ✅ Backup & Recovery successful
